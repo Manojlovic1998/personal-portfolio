23b2c5a9048e41865f767894bdb29e0f22605a8f
Nemanja <manojlovic911@hotmail.com>
2023-04-16 21:36:10 +0200

    Update configuration for work posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,7 +6,7 @@ export const collections = {
       title: z.string(),
       description: z.string(),
       publishDate: z.coerce.date(),
-      tags: z.array(z.string()),
+      tags: z.array(z.string()).optional(),
       img: z.string().optional(),
       img_preview: z.string().optional(),
       img_alt: z.string().optional(),
