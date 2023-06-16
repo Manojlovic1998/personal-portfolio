b79092151efc4e68d312d39d2ec3f76e8cb60042
Nemanja <manojlovic911@hotmail.com>
2023-06-17 00:41:59 +0200

    Add tools for image optimization

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,4 +1,16 @@
-#!/bin/bash
-
 # Install All Dependencies
 npm i
+
+echo "Cloning img-optimize project repo"
+git clone https://github.com/VirtuBox/img-optimize.git $HOME/.img-optimize
+
+echo "Creating img-optimize alias in $($HOME)/.bashrc ..."
+
+echo "alias img-optimize=$HOME/.img-optimize/optimize.sh" >> $HOME/.bashrc
+
+echo "Sourcing .bashrc file into current active bash env"
+. ~/.bashrc
+
+echo "----------------------------"
+echo "Installation Completed"
+echo "----------------------------"
