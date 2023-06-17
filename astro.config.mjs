398316f701edaff9fcc68f91e581de4ffa12a427
Nemanja Manojlovic <manojlovic911@hotmail.com>
2023-06-17 21:16:31 +0200

    Add support toolchain for tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site:"https://nemanjamanojlovic.com",
-  integrations: [mdx()]
+  site: "https://nemanjamanojlovic.com",
+  integrations: [mdx(), tailwind()]
 });
