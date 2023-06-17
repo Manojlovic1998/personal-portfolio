1f2d4c1b81fdbcf419f25e5339ac6ebb7d363aae
Nemanja <manojlovic911@hotmail.com>
2023-06-17 17:23:27 +0200

    Add support for DRACOLoader when loading GLTF files

--- src/utils/threejs/loadGLTFObject.ts
+++ src/utils/threejs/loadGLTFObject.ts
@@ -1,10 +1,18 @@
 import { GLTF, GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 import setupModel from "./setupModel";
 
 type ObjectSource = { url?: string; path?: string };
 
 const loadGLTFObject = async (objectSource: ObjectSource[]) => {
+  const dracoLoader = new DRACOLoader();
+  dracoLoader.setDecoderPath("https://www.gstatic.com/draco/versioned/decoders/1.5.6/");
+  await dracoLoader.preload();
+
   const loader = new GLTFLoader();
+
+  loader.setDRACOLoader(dracoLoader);
+
   // Create array of promisss to load one or more objects
   const promises: Promise<GLTF>[] = [];
   objectSource.map((object) => {
