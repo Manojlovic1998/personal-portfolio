e588d48844fe4793e8d5849d5a7f0d96dca656d2
Nemanja Manojlovic <manojlovic911@hotmail.com>
2023-09-30 08:49:27 +0000

    Format all of the files

--- src/utils/threejs/Resizer.ts
+++ src/utils/threejs/Resizer.ts
@@ -1,36 +1,44 @@
-const setSize = (container: HTMLElement, camera: THREE.PerspectiveCamera, renderer: THREE.WebGLRenderer) => {
-    // Set the camera's aspect ratio
-    camera.aspect = container.clientWidth / container.clientHeight;
-  
-    // Update the camera's frustum
-    camera.updateProjectionMatrix();
-  
-    // Update the size of the renderer AND the canvas
-    renderer.setSize(container.clientWidth, container.clientHeight);
-  
-    // Set the pixel ratio (for mobile devices)
-    renderer.setPixelRatio(window.devicePixelRatio);
-  };
+const setSize = (
+  container: HTMLElement,
+  camera: THREE.PerspectiveCamera,
+  renderer: THREE.WebGLRenderer,
+) => {
+  // Set the camera's aspect ratio
+  camera.aspect = container.clientWidth / container.clientHeight;
+
+  // Update the camera's frustum
+  camera.updateProjectionMatrix();
+
+  // Update the size of the renderer AND the canvas
+  renderer.setSize(container.clientWidth, container.clientHeight);
+
+  // Set the pixel ratio (for mobile devices)
+  renderer.setPixelRatio(window.devicePixelRatio);
+};
 
 class Resizer {
-    onResizeCallback?: () => void;
+  onResizeCallback?: () => void;
 
-    constructor(container: HTMLElement, camera: THREE.PerspectiveCamera, renderer: THREE.WebGLRenderer, onResizeCallback?: () => void) {
+  constructor(
+    container: HTMLElement,
+    camera: THREE.PerspectiveCamera,
+    renderer: THREE.WebGLRenderer,
+    onResizeCallback?: () => void,
+  ) {
+    setSize(container, camera, renderer);
+    this.onResizeCallback = onResizeCallback;
+
+    window.addEventListener("resize", () => {
+      // set the size again if a resize occurs
       setSize(container, camera, renderer);
-      this.onResizeCallback = onResizeCallback;
-  
-      window.addEventListener("resize", () => {
-        // set the size again if a resize occurs
-        setSize(container, camera, renderer);
-        // perform any custom actions
-        if (onResizeCallback) (this.onResize(onResizeCallback));
-      });
-    }
-  
-    onResize(onResizeCallback: () => void) {
-      onResizeCallback();
-    }
+      // perform any custom actions
+      if (onResizeCallback) this.onResize(onResizeCallback);
+    });
   }
 
+  onResize(onResizeCallback: () => void) {
+    onResizeCallback();
+  }
+}
 
 export default Resizer;
